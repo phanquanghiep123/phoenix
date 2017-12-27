6aaa999589b2836c36dc1d5b42457dd8e43a9ea6
phanquanghiep123 <phanquanghiep123@gmail.com>
2017-12-27 12:56:46 +0700

    â

--- system/core/http.js
+++ system/core/http.js
@@ -3,8 +3,12 @@ function Http(){
 	const server_ip_address = _Config.local.host;
 	const express           = require('express');
 	_App = express();
+	_App.use(express.cookieParser('phoneix'));
 	_App.use(express.session({ secret: 'phoneix' }));
 	_App.use(express.bodyParser());
+	_App.use(express.json());
+	_App.use(express.urlencoded());
+	_App.use(express.multipart());
 	_App.use("/skins",express.static(_Path + '/skins'));
 	_App.use("/uploads",express.static(_Path + '/uploads'));
 	_App.listen(server_port, server_ip_address, function () {
